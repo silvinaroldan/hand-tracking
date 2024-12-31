23c4070c1a135b6e0342d761f4ff2409c18b98b9
Silvina Roldan <silvinaroldan@gmail.com>
2024-12-30 22:25:06 -0300

    Remove await to add @MainActor

--- HandTracking/HandTracking/Models/HandTrackingViewModel.swift
+++ HandTracking/HandTracking/Models/HandTrackingViewModel.swift
@@ -20,7 +20,6 @@ final class HandTrackingViewModel {
     private var fingerEntities = [HandAnchor.Chirality: Entity]()
     
     func setupContentEntity() async -> Entity {
-        // fingerEntities[.left] = await createFingerTip()
         fingerEntities[.right] = await createFingerTip()
         for entity in fingerEntities.values {
             await contentEntity.addChild(entity)
@@ -30,9 +29,7 @@ final class HandTrackingViewModel {
     
     func startARKitSession() async throws {
         let result = await session.requestAuthorization(for: [.handTracking])
-        if let handAuthorization = result[.handTracking],
-           handAuthorization == .allowed
-        {
+        if let handAuthorization = result[.handTracking], handAuthorization == .allowed {
             try await session.run([handTracking])
         } else {
             print("ARKit is not supported or not authorized.")
@@ -62,14 +59,15 @@ final class HandTrackingViewModel {
         }
     }
     
+    @MainActor
     private func createFingerTip() async -> Entity {
         let animScene = try! await Entity(named: "butterfly", in: realityKitContentBundle)
         
-        await animScene.components.set(PhysicsBodyComponent(mode: .kinematic))
-        await animScene.components.set(OpacityComponent(opacity: 1.0))
+        animScene.components.set(PhysicsBodyComponent(mode: .kinematic))
+        animScene.components.set(OpacityComponent(opacity: 1.0))
         
-        if let animation = await animScene.availableAnimations.first {
-            await animScene.playAnimation(animation.repeat())
+        if let animation = animScene.availableAnimations.first {
+            animScene.playAnimation(animation.repeat())
         }
 
         return animScene
